7c4de16647f66d7c434aa4f12603d4a33817f9ab
HectorSuarez89 <hector.suarez89@hotmail.com>
2021-10-07 14:37:09 -0300

    Clean comment on test secretServiceTest

--- src/test/java/com/meli/quasar/Quasar/Operation/services/SecretServiceTest.java
+++ src/test/java/com/meli/quasar/Quasar/Operation/services/SecretServiceTest.java
@@ -42,46 +42,6 @@ public class SecretServiceTest {
 
     }
 
-    /*@Test
-    public void checkCalcutaleSecretWhenAllDataIsProvided() throws MessageException {
-        //Given
-        String[] msjkenobi = {"este", "","","mensaje",""};
-        String[] msjskywalker = {"", "es", "", "", "secreto"};
-        String[] msjsato = {"este", "", "un", "", "secreto"};
-
-        SatelliteDTO dtoKenobi = new SatelliteDTO("kenobi",100.0, msjkenobi);
-        SatelliteDTO dtoSkywalker = new SatelliteDTO("skywalker",115.5, msjskywalker);
-        SatelliteDTO dtoSato = new SatelliteDTO("sato",142.7, msjsato);
-
-        Satellite kenobi = new Satellite(1, "Kenobi", new Position(1, -500.0,-200.0));
-        Satellite skywalker = new Satellite(2, "skywalker", new Position(2, 100.0,-100.0));
-        Satellite sato = new Satellite(3, "sato", new Position(3, 500.0,100.0));
-
-        List<SatelliteDTO> satellites = List.of(dtoSato);
-
-        String expectedMessage = "este es un mensaje secreto";
-        double[] expectedPosition = {-58.31,-69.55};
-
-        given(satelliteService.findByName("sato"))
-                .willReturn(sato);
-        given(messageService.getMessage(dtoSato.getMessage()))
-                .willReturn(expectedMessage);
-        given(locationService.getLocation(dtoSato.getDistance()))
-                .willReturn(expectedPosition);
-
-        Position secretPosition = new Position();
-        secretPosition.setX(expectedPosition[0]);
-        secretPosition.setY(expectedPosition[1]);
-        Secret expectedSecret = new Secret();
-        expectedSecret.setPosition(secretPosition);
-        expectedSecret.setMessage(expectedMessage);
-
-        Secret secret = service.findSecret(satellites);
-
-        then(expectedSecret).isEqualTo(secret);
-
-    }*/
-
     @Test
     public void checkCorrectSecret() {
         //Given
